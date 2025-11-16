c32a4af718671c745ff79c0526830f009dbff997
Deepak Gaikwad <deepakgaikwad@Deepaks-MacBook-Pro.local>
2025-11-16 09:58:45 +0530

    used agent as a tool - two agents will talk to each other and resolve customer issues

--- src/routes/openAIRouter.js
+++ src/routes/openAIRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { chatWithAgent, chatWithGPT, chatWithWeatherAgentTool } from '../controller/openAIController.js';
+import { chatWithAgent, chatWithAgentManager, chatWithGPT, chatWithWeatherAgentTool } from '../controller/openAIController.js';
 
 export const openAIRouter = express.Router();
 
 openAIRouter.get('/chatgpt', chatWithGPT);
 openAIRouter.get('/agent', chatWithAgent);
 openAIRouter.get('/agentTool', chatWithWeatherAgentTool);
+openAIRouter.get('/agentManager', chatWithAgentManager);
+
 
 
